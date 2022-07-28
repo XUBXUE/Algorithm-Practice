00c73fadaf191a38fb3ed54d50ad3888627a9d28
xuboxue <2370237499@qq.com>
2022-07-28 10:47:15 +0800

    feat: twoSum

---
+++ 两数之和.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      return [map.get(nums[i]), i];
+    }
+    map.set(target - nums[i], i);
+  }
+}
